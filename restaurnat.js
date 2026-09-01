af8140adb61d9b968807a9f21c7c9fb3f84c1753
Erfandev88 <erfangaaang@gmail.com>
2026-09-01 22:40:56 +0330

    configs added

--- restaurnat.js
+++ restaurnat.js
@@ -1,10 +1,12 @@
 import express from "express";
 import router from './src/routes/index.js'
+import c from "config"
 
 const app = express();
+const port = c.get("port") || 3000
 
 app.use(express.json());
 
 app.use('/api', router)
 
-app.listen(3000, () => console.log("App is running on port 3000"));
+app.listen(port, () => console.log(`App is running on port ${port}`));
